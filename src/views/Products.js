339b8ad9ec8f9b9c6b5003f55c7b68bb15d8ae08
Matin_MAB <Matinmab789@gmail.com>
2022-02-26 20:18:02 +0330

    set Loading for times of receiving products

--- src/views/Products.js
+++ src/views/Products.js
@@ -1,23 +1,20 @@
-import React , {useContext} from 'react';
-import Product from '../components/Product';
+import React, { useContext } from "react";
+import Product from "../components/Product";
 
 //Styles
-import style from "../assets/css/products.module.css"
+import style from "../assets/css/products.module.css";
 
 //Context
-import { ProductsContext } from '../context/ProductContextProvider';
+import { ProductsContext } from "../context/ProductContextProvider";
 
 //Components
-
 
 const Products = () => {
   const products = useContext(ProductsContext);
 
   return (
     <div className={style.container}>
-      {
-        products.map(product => <Product key={product.id} productData={product} />)
-      }
+      {products.length === 0 ? <h2 style={{textAlign: 'center'}}>Loading Products<br/>Please Wait ...</h2> : products.map((product) => <Product key={product.id} productData={product} />)}
     </div>
   );
 };
